6e1ad4d80c9170e21dc1d3d0ae7c36cd8b70db5b
KyleMit <kmitofsky@stackoverflow.com>
2022-07-06 08:49:56 -0400

    ps1 profile

--- profile.ps1
+++ profile.ps1
@@ -13,3 +13,40 @@
 oh-my-posh init pwsh --config $env:USERPROFILE\.stacker.omp.yaml | Invoke-Expression
 
 
+function cdso() {
+    Set-Location -Path C:\code\StackOverflow
+}
+
+function dls() {
+    Push-Location C:/code/DevLocalSetup
+    git pull
+    ./setup.bat
+    Pop-Location
+}
+
+function migrate() {
+    $cwd=Get-Location
+    Set-Location C:/code/StackOverflow
+    git pull
+
+    Set-Location C:/code/StackOverflow/StackOverflow.Migrations
+    ./migrate.local.bat # --force
+
+    Set-Location C:/code/StackOverflow/Migrations
+    ./migrate.local.channels.bat # --force
+
+    Set-Location C:/code/StackOverflow/Sites.Migrations
+    ./migrate.local.bat # --force
+
+    Set-Location $cwd
+
+}
+
+function Start-AllAppPools {
+    if (!(Get-Module | Where-Object {$_.Name -eq 'IISAdministration'})) {
+        Import-Module IISAdministration
+    }
+
+    # TODO - need to elevate to do this...?
+    Get-IISAppPool | Where-Object { $_.State -eq 'Stopped' } | ForEach-Object { $_.Start() }
+}
