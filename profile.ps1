ad19495ed2082dbc5ef75fac0757b96fe9618911
KyleMit <kmitofsky@stackoverflow.com>
2022-12-02 12:45:13 -0500

    words and so build scripts

--- profile.ps1
+++ profile.ps1
@@ -64,3 +64,36 @@ function touch {
         New-Item -Type File -Path $Path
     }
 }
+
+function so-build {
+
+    $buildTimer = [Diagnostics.Stopwatch]::StartNew()
+    dotnet build C:\code\StackOverflow\StackOverflow.sln
+    $buildTimer.Stop()
+    # Start-IISAppPools # requires admin
+
+    # ping site
+    $pingTimer = [Diagnostics.Stopwatch]::StartNew()
+    $statusCode = Invoke-WebRequest "https://meta.stackexchange.local/" | Select-Object -ExpandProperty StatusCode
+    $pingTimer.Stop()
+
+    # ping user
+    # Install-Module -Name BurntToast
+    New-BurntToastNotification `
+        -AppLogo "C:\Users\kmitofsky\Pictures\windows\stack.png" `
+        -Text "Build completed", "Build: $([Math]::Floor($buildTimer.Elapsed.TotalSeconds))s Ping: $([Math]::Floor($pingTimer.Elapsed.TotalSeconds))s Status: $status"
+}
+
+function so-test-migrate {
+    # https://stackoverflowteams.com/c/stack-overflow/a/6046/675
+    dotnet run --project C:\code\StackOverflow\utils\StackOverflow.Setup\StackOverflow.Setup.csproj -- network remove PublicTest
+    dotnet run --project C:\code\StackOverflow\utils\StackOverflow.Setup\StackOverflow.Setup.csproj -- network new PublicTest
+}
+
+function so-test-integration {
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string]$testName
+    )
+    dotnet test C:\code\StackOverflow\tests\StackOverflow.Tests.Integration\StackOverflow.Tests.Integration.csproj --filter DisplayName~$testName
+}
